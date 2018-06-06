65f6be0c9b04133e54131c9008f783cba9583c94
CloudOne <munkeyten@protonmail.com>
2018-06-06 19:46:26 +0300

    adding additions to database

--- server.js
+++ server.js
@@ -20,10 +20,8 @@ const image = require('./controllers/image');
 const db = knex({
 		client: 'pg',
 		connection: {
-		  host : 'postgresql-parallel-51792',
-		  user : 'anestisfinstad',
-		  password : '',
-		  database : 'smart-brain'
+		  host : 'process.env.DATABASE_URL',
+		  ssl: true,
 		}
 });
 
